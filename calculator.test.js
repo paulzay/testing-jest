5eb9c5dec426099899fde79642b11033885fd5c3
paulzay <paulotieno2@gmail.com>
2020-10-27 10:57:19 +0300

    fix bug

--- calculator.test.js
+++ calculator.test.js
@@ -1,6 +1,6 @@
 import {add, subtract, multiply, divide } from './calculator';
 
-test('returns uppercased string', () =>{
+test('perfom computations', () =>{
 	expect(add(1,2)).toBe(3)
 	expect(subtract(4,2)).toBe(2);
 	expect(multiply(2,4)).toBe(8);
