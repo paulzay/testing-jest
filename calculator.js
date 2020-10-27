51e7984e1eb80e44bf2d22af58462f41a33f5571
paulzay <paulotieno2@gmail.com>
2020-10-27 10:48:56 +0300

    add calculator and its unit tests

---
+++ calculator.js
@@ -0,0 +1,16 @@
+
+export const add = (a,b) => {
+	return a + b;
+}
+
+export const  subtract = (a, b)=>{
+	return a - b;
+}
+
+export const multiply = (a, b) => {
+	return a * b;
+}
+
+export const divide = (a, b) => {
+	return a / b;
+}
---
+++ calculator.test.js
@@ -0,0 +1,8 @@
+import {add, subtract, multiply, divide } from './calculator';
+
+test('returns uppercased string', () =>{
+	expect(add(1,2)).toBe(3)
+	expect(subtract(4,2)).toBe(2);
+	expect(multiply(2,4)).toBe(8);
+	expect(divide(4,2)).toBe(2);
+})
