6cd5e9618d531118485783d7fe245b386f4feb44
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-10-27 14:18:06 +0600

    Add capitalize test and method

--- calculator.test.js
+++ calculator.test.js
@@ -1,7 +1,7 @@
 import {add, subtract, multiply, divide } from './calculator';
 
 test('perfom computations', () =>{
-	expect(add(1,2)).toBe(3)
+	expect(add(1,2)).toBe(3);
 	expect(subtract(4,2)).toBe(2);
 	expect(multiply(2,4)).toBe(8);
 	expect(divide(4,2)).toBe(2);
---
+++ capitalize.test.js
@@ -0,0 +1,5 @@
+import {capitalizeString} from './capitalize';
+
+test ('returns capitalized string', () => {
+  expect(capitalizeString('string')).toBe('String');
+})
